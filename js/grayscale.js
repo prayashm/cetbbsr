db6585ec940a5dd903b82beecf2f723068d7728e
prayashm <catchprayash@gmail.com>
2014-02-23 12:17:06 +0530

    Version 2

--- js/grayscale.js
+++ js/grayscale.js
@@ -20,8 +20,8 @@ $(function() {
 
 //Google Map Skin - Get more at http://snazzymaps.com/
 var myOptions = {
-    zoom: 15,
-    center: new google.maps.LatLng(53.385873, -1.471471),
+    zoom: 16,
+    center: new google.maps.LatLng(20.2757238, 85.7766258),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     disableDefaultUI: true,
     styles: [{
